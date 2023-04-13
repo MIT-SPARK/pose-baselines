982fd907f7011749a32787af616b8f4c17284abc
Rajat Talak <talak@mit.edu>
2023-04-13 17:29:04 -0400

    deleting PointNetLK_Revisited

--- PointNetLK_Revisited/scripts/test_3dmatch.sh
+++
@@ -1,8 +0,0 @@
-# cd ../
-
-python -W ignore test.py \
---outfile test_logs/3dmatch/ \
---dataset_path dataset/ThreeDMatch/ \
---categoryfile ./dataset/test_3dmatch.txt \
---dataset_type 3dmatch
-
--- PointNetLK_Revisited/scripts/train_shapenet.sh
+++
@@ -1,7 +0,0 @@
-
-python -W ignore train.py \
---dataset_type shapenet.real.hard \
---object all \
---pretrained './logs/model_trained_on_ModelNet40_model_best.pth' \
---writer True
-
